8c7e057b8ef2c8fac577fe642e8fac94ffba1cb0
Xiuyi Ye <xiuyiye@gmail.com>
2016-03-13 15:41:41 -0400

    added the scenarios that the table is full
    Situation 1: table is full and it doesn't contain that key
    Situation 2: table is full, you cannot add in new entries.

--- src/net/mv/HashMap.java
+++ src/net/mv/HashMap.java
@@ -3,6 +3,10 @@ package net.mv;
 public class HashMap {
 	private final static int TABLE_SIZE = 128;
 	HashEntry[] table;
+	/*
+	 * A boolean indicates if the table is full
+	 */
+	private boolean isFull = false;
 	
 	HashMap(){
 		table = new HashEntry[TABLE_SIZE];
@@ -12,9 +16,21 @@ public class HashMap {
 	}
 	
 	public int get(int key){
+		int cnt = 0;
 		int hash = (key%TABLE_SIZE);
-		while(table[hash]!=null&& table[hash].getKey()!=key)
+		while(cnt<TABLE_SIZE&&table[hash]!=null&& table[hash].getKey()!=key){
 			hash = (hash+1)%TABLE_SIZE;
+			cnt++;
+		}
+			
+		/*
+		 * The key doesn't exist and the table is full
+		 */
+		if(cnt>=TABLE_SIZE){
+			System.out.println("The key doesn't exist and the table is full");
+			isFull=true;
+			return -1;
+		}
 		if(table[hash]==null){
 			System.out.println("the value with that key hasn't been stored");
 			return -1;
@@ -24,8 +40,13 @@ public class HashMap {
 	}
 	
 	public void put(int key, int value){
+		int cnt =0;
 		if(key<0){
 			System.out.println("Key should be greater than 0");
+			return;
+		}
+		if(isFull){
+			System.out.println("Table is full");
 			return;
 		}
 		int hash= (key % TABLE_SIZE);
@@ -33,8 +54,20 @@ public class HashMap {
 			System.out.println("value with" + hash +" as key already exists!");
 			System.out.println("And the value is "+ table[hash].getValue());
 		}
-		while(table[hash]!= null&&table[hash].getKey()!=key)
+		while(cnt<TABLE_SIZE&&table[hash]!= null&&table[hash].getKey()!=key){
 			hash = (hash+1) % TABLE_SIZE;
+			cnt++;
+			
+		}
+			
+		/*
+		 * Table is full, and no new entries can be added.
+		 */
+		if(cnt>=TABLE_SIZE){
+			System.out.println("The table is full");
+			isFull=true;
+			return;
+		}
 		table[hash] = new HashEntry(key, value);
 		System.out.println("************");
 		System.out.println("new entry added with "+ "key = " + key + "value = " + value + "hash = "+ hash);
